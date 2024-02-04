99b61dd64f9c750f7d5f17f97c3cbb9b69ef695f
Nichlas Hoberg <nichlashoberg@gmail.com>
2024-02-05 00:20:40 +0100

    feat: Added Premake tool and initial project

---
+++ Build.lua
@@ -0,0 +1,17 @@
+-- premake5.lua
+workspace "EngineX"
+   architecture "x64"
+   configurations { "Debug", "Staging", "Release" }
+   startproject "App"
+
+   -- Workspace-wide build options for MSVC
+   filter "system:windows"
+      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+
+OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+
+group "Core"
+	include "Core/Build-Core.lua"
+group ""
+
+include "App/Build-App.lua"
